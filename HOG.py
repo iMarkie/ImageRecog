0cf92f19c4bed04edb7c9e84813d44cbae9f68f4
markiemark33 <38164774+markiemark33@users.noreply.github.com>
2018-04-17 06:46:41 +0000

    Update HOG.py

--- HOG.py
+++ HOG.py
@@ -1,9 +1,15 @@
+######
+# Example from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+######
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('dave.jpg',0)
 
+####
+# Image with CV_64F type to keep negative edge from light tot dark
+####
 laplacian = cv2.Laplacian(img,cv2.CV_64F)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
